024bca273d4afa0a722b496e6e1de3d478f9af49
Bojan Kolano <bojan19941@hotmail.com>
2019-12-30 23:07:59 +0100

    finished news and finished hashtag on desktop and tablet, mobile left to complete

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,22 @@
 
 $('document').ready(function(){
 
+  let image = $('.image');
+
+  for (let i = 0; i < image.length; i++) {
+
+    $(image[i]).mouseover((e) => {
     
-   
+      let inner = $(e.currentTarget).find('.block__inner');
+      console.log(inner);
+      inner.addClass('block__inner--show');
+    });
+
+    $(image[i]).mouseout((e) => {
+    
+      let inner = $(e.currentTarget).find('.block__inner');
+      console.log(inner);
+      inner.removeClass('block__inner--show');
+    });
+  }
 });
