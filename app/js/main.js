2704571a3094274f6ddf7d185ecefec19158040d
Bojan Kolano <bojan19941@hotmail.com>
2020-01-01 22:34:44 +0100

    started doing section moments slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,27 @@
 'use strict'
 
-$('document').ready(function(){
+$('document').ready(function() {
 
   let image = $('.image');
 
   for (let i = 0; i < image.length; i++) {
 
     $(image[i]).mouseover((e) => {
-    
+
       let inner = $(e.currentTarget).find('.block__inner');
       console.log(inner);
       inner.addClass('block__inner--show');
     });
 
     $(image[i]).mouseout((e) => {
-    
+
       let inner = $(e.currentTarget).find('.block__inner');
       console.log(inner);
       inner.removeClass('block__inner--show');
     });
   }
+
+  $('.block__moments__slider').slick({
+
+  })
 });
